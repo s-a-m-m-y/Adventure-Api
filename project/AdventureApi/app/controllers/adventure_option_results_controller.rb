1fc3c648a683b029fa11901c1bb9fe86255f5b83
Sam <samuel.h.littlefield>
2018-05-20 02:43:01 -0400

    Adding Adventure option results

---
+++ project/AdventureApi/app/controllers/adventure_option_results_controller.rb
@@ -0,0 +1,74 @@
+class AdventureOptionResultsController < ApplicationController
+  before_action :set_adventure_option_result, only: [:show, :edit, :update, :destroy]
+
+  # GET /adventure_option_results
+  # GET /adventure_option_results.json
+  def index
+    @adventure_option_results = AdventureOptionResult.all
+  end
+
+  # GET /adventure_option_results/1
+  # GET /adventure_option_results/1.json
+  def show
+  end
+
+  # GET /adventure_option_results/new
+  def new
+    @adventure_option_result = AdventureOptionResult.new
+  end
+
+  # GET /adventure_option_results/1/edit
+  def edit
+  end
+
+  # POST /adventure_option_results
+  # POST /adventure_option_results.json
+  def create
+    @adventure_option_result = AdventureOptionResult.new(adventure_option_result_params)
+
+    respond_to do |format|
+      if @adventure_option_result.save
+        format.html { redirect_to @adventure_option_result, notice: 'Adventure option result was successfully created.' }
+        format.json { render :show, status: :created, location: @adventure_option_result }
+      else
+        format.html { render :new }
+        format.json { render json: @adventure_option_result.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /adventure_option_results/1
+  # PATCH/PUT /adventure_option_results/1.json
+  def update
+    respond_to do |format|
+      if @adventure_option_result.update(adventure_option_result_params)
+        format.html { redirect_to @adventure_option_result, notice: 'Adventure option result was successfully updated.' }
+        format.json { render :show, status: :ok, location: @adventure_option_result }
+      else
+        format.html { render :edit }
+        format.json { render json: @adventure_option_result.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /adventure_option_results/1
+  # DELETE /adventure_option_results/1.json
+  def destroy
+    @adventure_option_result.destroy
+    respond_to do |format|
+      format.html { redirect_to adventure_option_results_url, notice: 'Adventure option result was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_adventure_option_result
+      @adventure_option_result = AdventureOptionResult.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def adventure_option_result_params
+      params.require(:adventure_option_result).permit(:reward, :adventure_option_id)
+    end
+end
--- project/AdventureApi/config/routes.rb
+++ project/AdventureApi/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :adventure_option_results
   resources :adventure_options
   resources :adventures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
