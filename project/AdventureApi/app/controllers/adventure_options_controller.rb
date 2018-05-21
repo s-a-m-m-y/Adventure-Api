5c2b023436e2dc71c5203b08ad7c218d943fbe2f
Sam <samuel.h.littlefield>
2018-05-21 14:38:42 -0400

    Setting an if/else for if a reward/result exists, otherwise you can't randomize results

--- project/AdventureApi/app/controllers/adventure_options_controller.rb
+++ project/AdventureApi/app/controllers/adventure_options_controller.rb
@@ -16,8 +16,13 @@ class AdventureOptionsController < ApplicationController
 
   def random_result
     @adventure_option_results = AdventureOptionResult.where( adventure_option_id: params[:adventure_option_id] )
-    @adventure_option_result = @adventure_option_results.offset(rand(@adventure_option_results.count)).first
-    redirect_to adventure_option_result_path(@adventure_option_result.id)
+    if @adventure_option_results.count > 0
+      @adventure_option_result = @adventure_option_results.offset(rand(@adventure_option_results.count)).first
+      redirect_to adventure_option_result_path(@adventure_option_result.id)
+    else
+      # need to do something here if option results don't exist
+      redirect_to adventure_option_result_path(0)
+    end
   end
 
   # GET /adventure_options/new
